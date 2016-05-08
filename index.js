8b7ed17f9cab7f7184534e811202198013f636ad
Jhen <developer@jhen.me>
2016-05-08 11:01:02 +0800

    Refactor

--- index.js
+++ index.js
@@ -17,45 +17,46 @@ module.exports = function(babel) {
         var scope = path.scope
         var callee = node.callee
 
-        if (!callee.property || callee.property.name !== 'push') return
-        if (node.arguments > 1) return
+        if (
+          !callee.property ||
+          callee.property.name !== 'push' || 
+          node.arguments > 1 ||
+          callee.object.type !== 'Identifier'
+        ) return
 
-        if (callee.object.type === 'Identifier') {
-          var binding = scope.getBinding(callee.object.name)
+        var binding = scope.getBinding(callee.object.name)
+        if (
+          !binding.path.isVariableDeclarator() ||
+          binding.constantViolations.length > 0 ||
+          binding.path.get('init').type !== 'ArrayExpression'
+        ) return
 
-          if (
-            binding.path.type !== 'VariableDeclarator' ||
-            binding.constantViolations.length > 0 ||
-            binding.path.get('init').type !== 'ArrayExpression'
-          ) return
-
-          var assignmentReplace = t.assignmentExpression(
-            '=',
+        var assignmentReplace = t.assignmentExpression(
+          '=',
+          t.memberExpression(
+            callee.object,
             t.memberExpression(
               callee.object,
+              t.identifier('length')
+            ),
+            true
+          ),
+          node.arguments[0]
+        )
+
+        if (types.indexOf(path.parentPath.type) !== -1) {
+          var expressionStatement = t.expressionStatement(
+            t.sequenceExpression([
+              assignmentReplace,
               t.memberExpression(
                 callee.object,
                 t.identifier('length')
-              ),
-              true
-            ),
-            node.arguments[0]
+              )
+            ])
           )
-
-          if (types.indexOf(path.parentPath.type) !== -1) {
-            var expressionStatement = t.expressionStatement(
-              t.sequenceExpression([
-                assignmentReplace,
-                t.memberExpression(
-                  callee.object,
-                  t.identifier('length')
-                )
-              ])
-            )
-            path.replaceWith(expressionStatement)
-          } else {
-            path.replaceWith(assignmentReplace)
-          }
+          path.replaceWith(expressionStatement)
+        } else {
+          path.replaceWith(assignmentReplace)
         }
       }
     }
